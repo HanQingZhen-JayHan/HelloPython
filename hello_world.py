cce5737ddcd706b555b73c98f38a05a3b2f84ece
Jay Han <45500484+HanQingZhen-JayHan@users.noreply.github.com>
2025-03-27 14:21:18 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -10,7 +10,53 @@ class HelloWorld:
 
     def get_msg(self):
         return self.msg
+        
+    def data_structure(self):
+        list = []
+        list.append(1)
+        for dig in list:
+            print(dig)
+        for index in range(0, len(list)):
+            print(list[index])
+            
+        dictionary = {"key":"value"}
+        dictionary["hello"]="world"
+        for (k,v) in dictionary.items():
+            print("key:{}, value:{}".format(k,v)
+        
+        _set=set()
+        _set.add(1)
+        for digit in _set:
+            print(digit)
 
+def _is_exist(self,str, policys):
+    for policy in policys:
+        result = re.search(policy, str)
+        if result:
+            return True
+        return False
+def check(self):
+    policys=["hello",
+             "world"]
+    return _is_exist("hell",policys)
+
+def extract(self):
+    str = "Hello (World)"
+    pattern = r"Hello \((\w+)\).*"
+    m = match(pattern,str)
+    if m and m.group(1) == "World":
+        return "Find {}".format(m.group(1))
+    return ''
+def read_file(self, file_path):
+    with open(file_path, 'rb') as f
+         return f.readlines()
+    return []
+    
+def parse_file(self, file_path):
+    lines = self.read_files(file_path)
+    for line in lines:
+        line = line.decode('ISO-8859-1')
+        print(line)
 if __name__ == '__main__':
     h = HelloWorld()
     h.print_msg()
